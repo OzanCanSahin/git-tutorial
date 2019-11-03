22fdb5628b5e95d3fed873b72f4cb0ad7a32beea
OzanCanSahin <ozancansahin94@gmail.com>
2019-11-03 13:25:52 +0100

    Wrote code for main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+#include "espl_lib.h"
 
 void main()
 {
-	printf("Hello ESPL \n");
-	printf("Another print statement \n");
+	char cont;
+
+	do
+	{
+		printf("Hello ESPL \n");
+
+		unsigned int UserNumber;
+		char* funcResult; 
+
+		printf("Enter a number: \n");
+		scanf("%d", &UserNumber);
+		funcResult = num_to_words(UserNumber);
+		printf("%c \n", &funcResult);
+
+		printf("Do you want to enter another number?(y/n) \n");
+		scanf("%c", &cont);
+
+	} while (cont == 'y');
+	
 }
